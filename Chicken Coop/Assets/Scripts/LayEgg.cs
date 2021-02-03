abe2d88520fed212d2cd9413a39acc5f83a41651
Thomas <quentin.sebring@icloud.com>
2021-02-02 21:39:07 -0600

    Added Scripts

---
+++ Chicken Coop/Assets/Scripts/LayEgg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayEgg : MonoBehaviour
+{
+
+    public GameObject egg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("layEgg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator layEgg()
+    {
+        for(; ; )
+        {
+            egg.transform.position = new Vector2(transform.position.x, transform.position.y);
+            Instantiate(egg);
+            yield return new WaitForSeconds(Random.Range(2f,5f));
+        }
+    }
+}
