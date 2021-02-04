3b183770828e857d0941785f9ad1932351b2e601
Thomas <quentin.sebring@icloud.com>
2021-02-04 01:06:31 -0600

    Added Scripts

---
+++ Chicken Coop/Assets/Scripts/CaughtSomething.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaughtSomething : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        if (collision.gameObject.tag == "Egg")
+        {
+            GameManager.Instance.IncScore(1);
+        }
+        if (collision.gameObject.tag == "Bomb")
+        {
+            GameManager.Instance.IncScore(-3);
+        }
+        
+    }
+}
---
+++ Chicken Coop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance {get; private set;}
+    private int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void IncScore(int ds)
+    {
+        score += ds;
+        scoreText.text = "Score: " + score;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
